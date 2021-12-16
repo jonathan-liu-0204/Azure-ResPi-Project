910d9761ec07a5e2bd084dd40ff969073a59fff1
Jonathan Liu (ManpowerGroup Taiwan) <a-jliu@microsoft.com>
2021-12-16 16:15:05 +0800

    add feature to upload to azure

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -4,6 +4,8 @@ import sys
 import time
 import subprocess
 
+from azure.storage.blob import BlockBlobService
+from azure.storage.blob import ContentSettings
 
 # read the absolute path
 script_dir = os.path.dirname(__file__)
@@ -13,6 +15,16 @@ os.system('./webcam.sh')
 currentdate = datetime.datetime.now().strftime("%Y-%m-%d_%H%M")
 # create the real path
 rel_path = currentdate +".jpg"
+
+block_blob_service = BlockBlobService(account_name='raspiazureproject', account_key='YOURKEY')
+block_blob_service.create_blob_from_path(
+    'webcamimageupload',
+    'firstblood.jpg',
+    rel_path,
+    content_settings=ContentSettings(content_type='image/jpeg'))
+print("file uploaded")
+
 #  join the absolute path and created file name
 abs_file_path = os.path.join(script_dir, rel_path)
+
 print abs_file_path
