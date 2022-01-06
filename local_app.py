ac52c66f7e289d98d30c08a1d41f891a1b93b6cf
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-01-06 15:31:04 +0800

    opening up a new directory

--- local_app.py
+++ local_app.py
@@ -4,6 +4,22 @@ import sys
 import time
 import subprocess
 
+#------------azure custom vision--------------
+
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
+from msrest.authentication import ApiKeyCredentials
+import os, time, uuid
+
+# Replace with valid values
+ENDPOINT = "PASTE_YOUR_CUSTOM_VISION_TRAINING_ENDPOINT_HERE"
+training_key = "PASTE_YOUR_CUSTOM_VISION_TRAINING_SUBSCRIPTION_KEY_HERE"
+prediction_key = "PASTE_YOUR_CUSTOM_VISION_PREDICTION_SUBSCRIPTION_KEY_HERE"
+prediction_resource_id = "PASTE_YOUR_CUSTOM_VISION_PREDICTION_RESOURCE_ID_HERE"
+
+#---------------------------------------------
+
 # read the absolute path
 script_dir = os.path.dirname(__file__)
 
@@ -17,3 +33,17 @@ rel_path = DATE +".jpg"
 #  join the absolute path and created file name
 abs_file_path = os.path.join(script_dir, rel_path)
 print(abs_file_path)
+
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+
+with open(os.path.join ("./capture", rel_path), "rb") as image_contents:
+
+    print(image_contents)
+    # results = predictor.classify_image(
+    #     project.id, publish_iteration_name, image_contents.read())
+
+    # # Display the results.
+    # for prediction in results.predictions:
+    #     print("\t" + prediction.tag_name +
+    #           ": {0:.2f}%".format(prediction.probability * 100))
