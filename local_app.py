729cdd7d725d946bb3580a4d24dd4df9f89a1dea
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-01-06 16:08:11 +0800

    fixing the cv model read file problem

--- local_app.py
+++ local_app.py
@@ -30,7 +30,7 @@ os.system('fswebcam -r 1280x720 --no-banner ./captured/' + DATE + '.jpg')
 rel_path = DATE + ".jpg"
 
 #  join the absolute path and created file name
-abs_file_path = os.path.join(script_dir, "/captured", rel_path)
+abs_file_path = os.path.join(script_dir + "/captured", rel_path)
 print("absolute:    " + abs_file_path)
 
 # prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
