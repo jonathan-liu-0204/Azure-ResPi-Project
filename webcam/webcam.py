6d859b443997e8d9ba08d3b43b4a1dc1eb90c599
Jonathan Liu (ManpowerGroup Taiwan) <a-jliu@microsoft.com>
2022-01-06 11:24:51 +0800

    setting up local device file

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -1,10 +1,8 @@
 import os
 import datetime
-import sys
-import time
-import subprocess
 
-from azure.storage.blob import BloBcLIENT
+
+from azure.storage.blob import BlockBlobService
 
 # read the absolute path
 script_dir = os.path.dirname(__file__)
@@ -15,15 +13,3 @@ currentdate = datetime.datetime.now().strftime("%Y-%m-%d_%H%M")
 # create the real path
 rel_path = currentdate +".jpg"
 
-block_blob_service = BlockBlobService(account_name='raspiazureproject', account_key='YOURKEY')
-block_blob_service.create_blob_from_path(
-    'webcamimageupload',
-    'firstblood.jpg',
-    rel_path,
-    content_settings=ContentSettings(content_type='image/jpg'))
-print("file uploaded")
-
-# #  join the absolute path and created file name
-# abs_file_path = os.path.join(script_dir, rel_path)
-
-# print(abs_file_path)
