eac5c76953920a3f97d5de7dbb1022aa95480056
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-01-06 15:47:09 +0800

    fixing the cv model read file problem

--- local_app.py
+++ local_app.py
@@ -39,11 +39,11 @@ predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 
-print(os.path.join(CURR_DIR, "./captured", rel_path))
+#print(os.path.join(CURR_DIR, "./captured", rel_path))
 
-# with open(os.path.join(path, "./captured", rel_path), "rb") as image_contents:
+with open(os.path.join(CURR_DIR, "./captured", rel_path), "rb") as image_contents:
 
-#     print(image_contents)
+     print(image_contents)
 #     results = predictor.classify_image(project.id, publish_iteration_name, image_contents.read())
 
 #     #Display the results.
