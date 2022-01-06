27b2ee5af7332d4d2d1e0182a6155225411b6050
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-01-06 11:34:33 +0800

    modifying the local camera file

--- local_app.py
+++ local_app.py
@@ -1,18 +1,19 @@
 import os
-import datetime
+from datetime import datetime
 import sys
 import time
 import subprocess
 
 # read the absolute path
 script_dir = os.path.dirname(__file__)
+
 # call the .sh to capture the image
-DATE = datetime.now()
+DATE = datetime.now().strftime("%Y-%m-%d_%H%M")
 os.system('fswebcam -r 1280x720 --no-banner /home/pi/webcam/$' + DATE + '.jpg')
-#get the date and time, set the date and time as a filename.
-currentdate = datetime.datetime.now().strftime("%Y-%m-%d_%H%M")
+
 # create the real path
-rel_path = currentdate +".jpg"
+rel_path = DATE +".jpg"
+
 #  join the absolute path and created file name
 abs_file_path = os.path.join(script_dir, rel_path)
 print(abs_file_path)
