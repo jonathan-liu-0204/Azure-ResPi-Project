8b234a69c477cb1901e79e287c47992557736b98
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-01-07 16:25:36 +0800

    attempt to connect azure storage

--- local_app.py
+++ local_app.py
@@ -27,47 +27,16 @@ predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
 
 #---------------------------------------------
 #----------------azure blob storage-------------
-from azure.storage.blob import BlobServiceClient, BlobClient
-from azure.storage.blob import ContentSettings, ContainerClient
+from azure.storage.blob import BlobClient
 
 # IMPORTANT: Replace connection string with your storage account connection string
 # Usually starts with DefaultEndpointsProtocol=https;...
-MY_CONNECTION_STRING = "REPLACE_THIS"
+CONNECTION_STRING = "REPLACE_THIS"
  
-# Replace with blob container. This should be already created in azure storage.
-MY_IMAGE_CONTAINER = "myimages"
+#blob_client = BlobClient(conn_string=CONNECTION_STRING,container_name="capturedimages",blob_name="testing.txt")
 
 #---------------------------------------------
 
-class AzureBlobFileUploader:
-  def __init__(self):
-    print("Intializing AzureBlobFileUploader")
- 
-    # Initialize the connection to Azure storage account
-    self.blob_service_client =  BlobServiceClient.from_connection_string(MY_CONNECTION_STRING)
- 
-  def upload_all_images_in_folder(self):
-    # Get all files with jpg extension and exclude directories
-    all_file_names = [f for f in os.listdir(LOCAL_IMAGE_PATH)
-                    if os.path.isfile(os.path.join(LOCAL_IMAGE_PATH, f)) and ".jpg" in f]
- 
-    # Upload each file
-    for file_name in all_file_names:
-      self.upload_image(file_name)
- 
-  def upload_image(self,file_name):
-    # Create blob with same name as local file name
-    blob_client = self.blob_service_client.get_blob_client(container=MY_IMAGE_CONTAINER,
-                                                          blob=file_name)
-    # Get full path to the file
-    upload_file_path = os.path.join(LOCAL_IMAGE_PATH, file_name)
- 
-    # Create blob on storage
-    # Overwrite if it already exists!
-    image_content_setting = ContentSettings(content_type='image/jpeg')
-    print(f"uploading file - {file_name}")
-    with open(upload_file_path, "rb") as data:
-      blob_client.upload_blob(data,overwrite=True,content_settings=image_content_setting)
 
 # read the absolute path
 script_dir = os.path.dirname(os.path.realpath(__file__))
